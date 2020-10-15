322152cd2515b17f1e3a3138dc0d1e50a31dcefa
WWHash <Reshetnikov.Hash@yandex.ru>
2020-10-15 13:27:48 +0300

    module5-task1: модуляция

---
+++ js/form.js
@@ -0,0 +1,48 @@
+"use strict";
+
+(function () {
+  const capacityInput = document.querySelector(`[name = capacity]`);
+  const roomsInput = document.querySelector(`[name= rooms]`);
+  const adForm = document.querySelector(`.ad-form`);
+  const fieldsets = adForm.querySelectorAll(`fieldset`);
+  const addressInput = adForm.querySelector(`[name = address]`);
+
+  const setAddress = function (x, y) {
+    addressInput.value = `${x}, ${y}`;
+  };
+
+  const activateForm = function () {
+    adForm.classList.remove(`ad-form--disabled`);
+    window.util.toggleDisabled(fieldsets, false);
+    onFormEditChange();
+  };
+
+  const deactivateForm = function () {
+    adForm.classList.add(`ad-form--disabled`);
+    window.util.toggleDisabled(fieldsets, true);
+  };
+
+  const validateRooms = function () {
+    if (roomsInput.value < capacityInput.value && capacityInput.value > 0 && roomsInput.value !== `100`) {
+      capacityInput.setCustomValidity(`Вам нужна квартира побольше`);
+    } else if (roomsInput.value === `100` && capacityInput.value > 0) {
+      capacityInput.setCustomValidity(`Эти аппартаменты не для гостей`);
+    } else if (roomsInput.value !== `100` && capacityInput.value === `0`) {
+      capacityInput.setCustomValidity(`Выберите аппартаменты не для гостей`);
+    } else {
+      capacityInput.setCustomValidity(``);
+    }
+  };
+
+  const onFormEditChange = function () {
+    validateRooms();
+  };
+
+  adForm.addEventListener(`change`, onFormEditChange);
+
+  window.form = {
+    activate: activateForm,
+    deactivate: deactivateForm,
+    setAddress
+  };
+})();
---
+++ js/map.js
@@ -0,0 +1,48 @@
+"use strict";
+
+(function () {
+  const mainPin = document.querySelector(`.map__pin--main`);
+  const mapFilters = document.querySelector(`.map__filters`);
+  const selectedFilters = mapFilters.querySelectorAll(`select`);
+
+  const setAddress = function (isDefault) {
+    let coordinate = mainPin.getBoundingClientRect();
+    let coordinateLeft = parseInt(mainPin.style.left, 10);
+    let coordinateTop = parseInt(mainPin.style.top, 10);
+    let x = Math.round(coordinateLeft + coordinate.width / 2);
+    let y = (isDefault) ? y = Math.round(coordinateTop + coordinate.height / 2) : y = Math.round(coordinateTop + coordinate.height);
+    window.form.setAddress(x, y);
+  };
+
+  const activatePage = function () {
+    window.data.map.classList.remove(`map--faded`);
+    window.util.toggleDisabled(selectedFilters, false);
+    setAddress();
+    window.pin.drawPins(window.data.offers);
+    window.form.activate();
+  };
+
+  const deactivatePage = function () {
+    window.data.map.classList.add(`map--faded`);
+    window.util.toggleDisabled(selectedFilters, true);
+    setAddress(true);
+    window.pin.deletePins();
+    window.form.deactivate();
+  };
+
+  const onMainPinClick = function (evt) {
+    if (evt.button === 0) {
+      activatePage();
+    }
+  };
+
+  const onMainPinEnter = function (evt) {
+    if (evt.key === `Enter`) {
+      activatePage();
+    }
+  };
+
+  mainPin.addEventListener(`mousedown`, onMainPinClick);
+  mainPin.addEventListener(`keydown`, onMainPinEnter);
+  deactivatePage();
+})();
