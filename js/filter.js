2602425bcd34fd7d0e60e6b19a73611bffa3c025
WWHash <Reshetnikov.Hash@yandex.ru>
2020-11-12 15:12:48 +0300

    module7-task2: на проверку

--- js/filter.js
+++ js/filter.js
@@ -1,12 +1,62 @@
 "use strict";
 
 (function () {
-  const housingTypeSelect = document.querySelector(`#housing-type`);
+  const lowPrice = 10000;
+  const highPrice = 50000;
+  const MAX_PINS_ON_THE_MAP = 5;
+  const filters = document.querySelector(`.map__filters`);
+  const filterType = filters.querySelector(`#housing-type`);
+  const filterPrice = filters.querySelector(`#housing-price`);
+  const filterRooms = filters.querySelector(`#housing-rooms`);
+  const filterGuests = filters.querySelector(`#housing-guests`);
+  const filterFeatures = filters.querySelectorAll(`input`);
 
-  housingTypeSelect.addEventListener(`change`, function (evt) {
-    const filteredOffers = window.map.getOriginalOffers().filter(function (offer) {
-      return offer.offer.type === evt.target.value || evt.target.value === `any`;
-    });
+  const comparePrice = function (value, price) {
+    switch (value) {
+      case `any`:
+        return true;
+      case `low`:
+        return price < lowPrice;
+      case `middle`:
+        return price >= lowPrice && price <= highPrice;
+      case `high`:
+        return price > highPrice;
+      default:
+        return false;
+    }
+  };
+
+  const compareInput = function (offerFeatures) {
+    for (let i = 0; i < filterFeatures.length; i++) {
+      if (filterFeatures[i].checked) {
+        if (!offerFeatures.includes(filterFeatures[i].value)) {
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+
+  const onFilterFormChange = function () {
+    const originalOffers = window.map.getOriginalOffers();
+    const filteredOffers = [];
+    for (const offer of originalOffers) {
+      if ((offer.offer.type === filterType.value || filterType.value === `any`)
+        && (`${offer.offer.rooms}` === filterRooms.value || filterRooms.value === `any`)
+        && (`${offer.offer.guests}` === filterGuests.value || filterGuests.value === `any`)
+        && comparePrice(filterPrice.value, offer.offer.price)
+        && compareInput(offer.offer.features)) {
+        filteredOffers.push(offer);
+        if (filteredOffers.length === MAX_PINS_ON_THE_MAP) {
+          break;
+        }
+      }
+    }
+
+    window.card.closeCard();
     window.pin.drawPins(filteredOffers);
-  });
+  };
+
+  filters.addEventListener(`change`, window.debounce(onFilterFormChange));
+
 })();
